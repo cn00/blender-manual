28a923c6ba73a7699d5793b6440fa9c5588a5097
cn <cool_navy@qq.com>
2020-04-27 18:09:53 +0800

    zh-hans-2.82-r6267

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '2.82',
-    LANGUAGE: 'en',
+    LANGUAGE: 'zh-hans',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
